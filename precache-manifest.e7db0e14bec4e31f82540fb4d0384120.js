c046b5af88daa51a21f4e4f249d3e4b95fb807f9
Quentin Chevrin <qchevrin@pictet.com>
2019-05-08 15:21:15 +0200

    Updates

---
+++ precache-manifest.e7db0e14bec4e31f82540fb4d0384120.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cb9b8bcbb91cb84a691ba21f33127a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73173802e42662d0592e",
+    "url": "/static/css/main.e3716b70.chunk.css"
+  },
+  {
+    "revision": "1d1c687796962b1f7fbe",
+    "url": "/static/js/2.58e9da8e.chunk.js"
+  },
+  {
+    "revision": "73173802e42662d0592e",
+    "url": "/static/js/main.a818ab67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
